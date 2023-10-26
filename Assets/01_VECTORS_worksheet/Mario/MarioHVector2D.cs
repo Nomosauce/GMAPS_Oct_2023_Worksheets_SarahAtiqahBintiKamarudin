c1d42736efe730b6816125e3ce6b1e652d327eab
Nomosauce <sarah_atiqah05@hotmail.com>
2023-10-26 23:07:10 +0800

    Q4b cont

--- Assets/01_VECTORS_worksheet/Mario/MarioHVector2D.cs
+++ Assets/01_VECTORS_worksheet/Mario/MarioHVector2D.cs
@@ -22,6 +22,19 @@ public class MarioHVector2D : MonoBehaviour
         gravityDir = new HVector2D(planet.position - transform.position);  
         moveDir = new HVector2D(gravityDir.y, -gravityDir.x);
 
-        moveDir.Normalize();
+        moveDir.Normalize(); //normalise the move direction before flipping pos/neg values
+        moveDir = moveDir * -1;
+
+        rb.AddForce(moveDir.ToUnityVector2() * force); //and convert to unitys vector 2d
+
+        gravityNorm = gravityDir;
+        gravityNorm.Normalize(); //normalised gravity direction to set gravity strength
+        rb.AddForce(gravityNorm.ToUnityVector2() * gravityStrength);
+
+        float angle = Vector3.SignedAngle(Vector3.right, moveDir.ToUnityVector3(), Vector3.forward); //calculate angle and convert to unitys vector 3d
+        rb.MoveRotation(Quaternion.Euler(0, 0, angle)); //rotates astronauts rb component
+
+        DebugExtension.DebugArrow(transform.position, gravityNorm.ToUnityVector3() * gravityStrength); //drawing the arrows
+        DebugExtension.DebugArrow(transform.position, moveDir.ToUnityVector3());
     }
 }
